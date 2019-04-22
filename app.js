74acbc31ac559423f59c325e5c26ae4472a08145
Unknown <dwirandyherdinanto@gmail.com>
2019-04-22 12:30:28 +0700

    add sequelize mysql

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
 import router from './src/routes/web'
 import hbs from 'express-handlebars';
 import path from 'path';
 
 const app = express();
 
+app.use(morgan('combined'))
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(router);
+
 
 app.engine('hbs', hbs(
     {
@@ -16,5 +26,6 @@ app.engine('hbs', hbs(
 ));
 app.set('views', path.join(__dirname, '/src/views'));
 app.set('view engine', 'hbs');
+
 
 export default app;
--- src/controllers/WelcomeController.js
+++ src/controllers/WelcomeController.js
@@ -1,8 +1,22 @@
 import express from 'express';
+import {Todo, TodoItem} from '../../database/models';
 
 class WelcomeController {
     async index(req, res){
-        res.render('welcome', { title: 'Express ES 6 MVC Boilperplate'});
+        const getTodo = Todo.findOne();
+        const getTodoItems = getTodo.then(res => {
+            return TodoItem.findAll({
+                where: {todoId: res.id}
+            })
+        })
+
+        // promise all
+        Promise.all([getTodo, getTodoItems]).then(result => {
+            var todo = result[0];
+            var todoItems = result[1];
+            res.render('welcome', { title: todo.title });
+        });
+        
     }
 }
 
